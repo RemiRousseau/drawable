2163907f55ea6c2bf18ecca29da2cf0130289893
Remi Rousseau <r_remi@hotmail.fr>
2023-02-10 16:16:16 +0100

    Lintering

--- drawable/drawable_element.py
+++ drawable/drawable_element.py
@@ -1,15 +1,23 @@
 import yaml
-from typing import List, Union
+from typing import List
 
 from HFSSdrawpy import Modeler, Body
 from HFSSdrawpy.utils import parse_entry
+
 
 class DrawableElement:
     parent = None
     name: str
     to_draw: bool = True
 
-    def __init__(self, dict_params: dict, modeler: Modeler, name: str, parent = None) -> None:
+    def __init__(
+        self,
+        dict_params: dict,
+        modeler: Modeler,
+        name: str,
+        parent=None
+    ) -> None:
+
         self.parent = parent
         self.name = name
 
@@ -19,12 +27,13 @@ class DrawableElement:
                 if self.__annotations__[k] in [str, int]:
                     if k[-4:] == "__np":
                         setattr(self, k, dict_params[k])
-                    elif k[-3:] == "__n" or modeler.mode=="gds":
+                    elif k[-3:] == "__n" or modeler.mode == "gds":
                         setattr(self, k, parse_entry(dict_params[k]))
                     else:
                         setattr(
-                            self, k, 
-                            modeler.set_variable(dict_params[k], name=name + "_" + k))
+                            self, k,
+                            modeler.set_variable(
+                                dict_params[k], name=name + "_" + k))
                 else:
                     attr_to_set.append(k)
             elif k not in ["parent", "name"]:
@@ -36,20 +45,23 @@ class DrawableElement:
                         if parent.parent is not None:
                             parent = parent.parent
                         else:
-                            raise KeyError(f"Key {k} should be defined in .yaml file in {self.__class__.__name__} or it's parents.")
+                            raise KeyError(
+                                f"Key {k} should be defined in .yaml file "
+                                + f"in {self.__class__.__name__} or "
+                                + "in it's parents.")
         for k in attr_to_set:
-            setattr(self, k, self.__annotations__[k](dict_params[k], modeler, name + "_" + k, parent=self))
-    
+            setattr(
+                self, k, self.__annotations__[k](
+                    dict_params[k], modeler, name + "_" + k, parent=self))
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__}: {self.name}>"
-    
 
     def __str__(self) -> str:
         string = ""
         for k in self.__dict__:
             attr = self.__dict__[k]
-            if k!="parent":
+            if k != "parent":
                 if isinstance(attr, DrawableElement):
                     repr_subel = str(attr)
                     repr_subel = repr_subel.replace("\n", "\n  ")
@@ -61,19 +73,21 @@ class DrawableElement:
                         string += f"{k}: {attr},\n"
         return f"{string[:-2]}"
 
-
     @property
     def children(self) -> List:
-        return [k for k in self.__dict__ if (isinstance(self.__dict__[k], DrawableElement) and k!="parent")]
-    
+        childs = []
+        for k in self.__dict__:
+            if isinstance(self.__dict__[k], DrawableElement) and k != "parent":
+                childs.append(k)
+        return childs
 
     def _draw(self, chip: Body, **kwargs) -> None:
         for k in self.children:
-                attribute = getattr(self, k)
-                attribute.draw(chip, **kwargs)
-        if len(self.children)==0:
-            raise NotImplementedError(f"_draw() of {self.__class__.__name__} is not implemented")
-
+            attribute = getattr(self, k)
+            attribute.draw(chip, **kwargs)
+        if len(self.children) == 0:
+            raise NotImplementedError(
+                f"_draw() of {self.__class__.__name__} is not implemented")
 
     def draw(self, chip: Body, **kwargs) -> None:
         if self.to_draw:
